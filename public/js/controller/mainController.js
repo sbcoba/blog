d3bf042793393cb9d50847313f0cc6a77075526a
동준 <mayajuni@naver.com>
2014-11-20 12:50:20 +0900

    메인화면 다시 개발 진행.
    아날로그 시계 제작

--- public/js/controller/mainController.js
+++ public/js/controller/mainController.js
@@ -2,7 +2,7 @@
  * Created by 동준 on 2014-11-14.
  */
 angular.module('johayo.controller')
-    .controller('mainController', ['$scope',
-        function($scope){
-            $scope.menuList = '';
+    .controller('mainController', ['$scope', '$timeout',
+        function($scope, $timeout){
+
         }]);
