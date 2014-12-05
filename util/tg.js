4b1192ebaf9dc04d6067a9e9655c317d3adad11b
동준 <mayajuni@naver.com>
2014-12-05 11:23:18 +0900

    메세지 부분 수정

--- util/tg.js
+++ util/tg.js
@@ -7,7 +7,6 @@ var config = require('../config/config');
 
 exports.sendMsg = function(msg){
     var _cmd = 'msg ' + 'dongjun_kwon ' +  msg;
-    console.log("echo " + _cmd  + " | "+config.tg.path+"bin/telegram-cli -k "+config.tg.path+"/tg-server.pub -W" );
     exec( "echo " + _cmd  + " | "+config.tg.path+"bin/telegram-cli -k "+config.tg.path+"/tg-server.pub -W" ,
         function(err, stdout, stderr) {
             if(err) {
