bea4949e7f1ef14071634a85ce230469c3cc5bc6
동준 <mayajuni@naver.com>
2014-12-11 17:09:43 +0900

    텔레그램 메세지 변경

--- routes/msg.js
+++ routes/msg.js
@@ -17,15 +17,16 @@ var router = express.Router();
  * 구분에 맞는 게시물을 가지고 온다.
  */
 router.post('/', function(req, res){
+    var content = validator.isNull(req.param('content'))  ? error.throw(4019,'Please check content.') : req.param('content');
     var tgData = {
         email : validator.isNull(req.param('email'))  ? error.throw(4019,'Please check email.') : req.param('email'),
-        content : '[Blog 메세지]'+validator.isNull(req.param('content'))  ? error.throw(4019,'Please check content.') : req.param('content'),
+        content : '[Blog 메세지]'+ content,
         division : 'msg',
         ip: req.headers['x-forwarded-for'] || req.ip
     };
 
     tg.sendMsg(tgData);
-    mail.sendMail(config.email.to, tgData.email + ' 님이 보낸 메세지', tgData.content);
+    mail.sendMail(config.email.to, tgData.email + ' 님이 보낸 메세지', content);
 
     res.send('');
 });
